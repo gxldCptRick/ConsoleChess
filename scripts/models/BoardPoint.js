0ae9ff67b3006ff865ffae7ed2250f57e37a22c3
ANDRES H CARRERA <andreshcar@live.com>
2018-08-01 11:38:34 -0600

    adding some basic functionlity to the BoardPoint class specifically saving the points passed into the constructor.

--- scripts/models/BoardPoint.js
+++ scripts/models/BoardPoint.js
@@ -1,6 +1,13 @@
 const ValueForA = 65;
 export class BoardPoint{
     constructor(alphaCharacter, numberedPosition){
+        var alphalpha = alphaCharacter.toUpperCase();
+        this.x = alphaCharacter.charCodeAt(0) - ValueForA;
+        if(this.x > 7 || this.x < 0) throw "Character Is out of bounds";
+        this.y = numberedPosition;
+    }
 
+    get Point(){
+        return String.fromCharCode(this.x + ValueForA) + this.y.toString();
     }
 }
