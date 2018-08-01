19e705289625f7df955d1dcfd7885aa4db1a987a
ANDRES H CARRERA <andreshcar@live.com>
2018-07-31 18:24:34 -0600

    doing some stuff in the index.js but nothing concrete

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,6 @@
-import * as ChessPiece from "./models/ChessPiece";
-var queen = new ChessPiece("White", "Queen");
+import { GameBoard } from './controllers/GameBoard';
+import { ChessPiece, PieceColor, PieceTypes } from './models/ChessPiece';
+var queen = new ChessPiece(PieceColor.White, PieceTypes.Queen);
+console.log(queen.type.value)
+var Board = new GameBoard();
+console.log(Board)
