93fa26cfee52a7fc66bd3ce37172bc2468ad6732
ANDRES H CARRERA <andreshcar@live.com>
2018-07-31 18:26:52 -0600

    adding an initial test that does nothing as of yet

---
+++ tests/test.js
@@ -0,0 +1,8 @@
+const mocha = require('mocha'),
+assert = require('assert');
+const { ChessPiece } = require('../lib/models/ChessPiece'); 
+mocha.describe('Test 1', function(){
+    mocha.it('Test Subtext', function(){
+        assert.equal();
+    });
+});
