d7aef8d51fb25d65c3320b420bc7a75e5063645d
ANDRES H CARRERA <andreshcar@live.com>
2018-08-01 09:55:21 -0600

    writing the first test for the pawn chess piece

--- tests/test.js
+++ tests/test.js
@@ -1,8 +1,15 @@
 const mocha = require('mocha'),
 assert = require('assert');
-const { ChessPiece } = require('../lib/models/ChessPiece'); 
-mocha.describe('Test 1', function(){
-    mocha.it('Test Subtext', function(){
-        assert.equal();
+const { ChessPiece, PieceTypes } = require('../lib/models/ChessPiece'); 
+const { BoardPoint } = require('../lib/models/BoardPoint');
+
+mocha.describe('Chess Piece Checkings', function(){
+    mocha.it('Pawn Can Move two times on first move', function(){
+        var chessPiece = new ChessPiece(null, PieceTypes.Pawn);
+        
+        chessPiece.setPosition(new BoardPoint('E',4));
+
+        assert.equal(chessPiece.moveTo(new BoardPoint('E', 6)), true);
+
     });
 });
