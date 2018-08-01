12c1f9f1244aae9fba77d20053cd428c3607c3e1
ANDRES H CARRERA <andreshcar@live.com>
2018-07-31 18:26:07 -0600

    updating the chesspiece object to finally be used and exported correctly

--- scripts/models/ChessPiece.js
+++ scripts/models/ChessPiece.js
@@ -1,18 +1,38 @@
-class ChessPiece
-{
+export class ChessPiece{
     constructor(color, type) {
-        this.height = color;
-        this.width = type;
-        this.position = null;
-    }
-
-    get position(){ 
-        return this.position;
-    } 
-
-    set position(newPosition){ 
-        this.position = newPosition;
+        this.color = color;
+        this.type = type;
+        var _position = null;
+        this.getPosition = () => _position;
+        this.setPosition = (value) => _position = value;
     } 
 }
 
-export default ChessPiece;
+var PieceType = function(name, value)
+{
+    var typeOfPiece = {}
+    Object.defineProperty(typeOfPiece, "name", {
+        value: name,
+        writeable: false
+    })
+    Object.defineProperty(typeOfPiece, "value",
+    {
+        value: value,
+        writable: false
+    });
+    return typeOfPiece;
+}
+
+const PieceTypes = {
+    Pawn: PieceType("Pawn", 1),
+    Bishop: PieceType("Bishop", 2),
+    Knight: PieceType("Knight", 3),
+    Rook: PieceType("Rook", 4),
+    Queen: PieceType("Queen", 5),
+    King: PieceType("King", 6)
+};
+export const PieceColor = {
+    White: "White",
+    Black: "Black" 
+};
+export { PieceTypes }
