534da3b58b0c2539bea0bdf0939a0b179b0098be
ANDRES H CARRERA <andreshcar@live.com>
2018-08-02 12:52:07 -0600

    removed the responsiblity of the BoardPoint class to check if it is a valid point

--- scripts/models/BoardPoint.js
+++ scripts/models/BoardPoint.js
@@ -1,12 +1,11 @@
 const ValueForA = 65;
-export class BoardPoint{
+export class BoardPoint {
     constructor(alphaCharacter, numberedPosition){
         var alphalpha = alphaCharacter.toUpperCase();
         this.x = alphalpha.charCodeAt(0) - ValueForA;
-        if(this.x > 7 || this.x < 0) throw "Character Is out of bounds";
         this.y = numberedPosition;
     }
-
+    
     get Point(){
         return String.fromCharCode(this.x + ValueForA) + this.y.toString();
     }
