6092b73c533389c54e7c6800c42ed6dccaee0354
Fernando Blanco <fern.alej.blanco@gmail.com>
2018-08-02 14:09:35 -0600

    Finished Knight.CanMoveTo function logic (For now)

--- scripts/models/ChessPiece.js
+++ scripts/models/ChessPiece.js
@@ -49,19 +49,37 @@ PieceTypes.Rook.canMoveTo = (currentPosition, nextPosition) => {
     return true;
 }
 
-PieceTypes.Bishop.canMoveTo = (currentPosition, nextPosition) =>{
+//Bishop Movement
+PieceTypes.Bishop.canMoveTo = (currentPosition, nextPosition) =>
+{
 var difInX = Math.abs(nextPosition.x - currentPosition.x);
 var difInY = Math.abs(nextPosition.y - currentPosition.y);
 //D4 >> C5
 //D4 >> E5
+//D4 >> C3
+//D4 >> E3
 var canMoveDiagonal = difInX > 0 && difInY > 0;
-
 return canMoveDiagonal;
 }
 
 PieceTypes.Knight.canMoveTo = (currentPosition, nextPosition) =>{
-    return true;
+    var difInX = Math.abs(nextPosition.x - currentPosition.x);
+    var difInY = Math.abs(nextPosition.y - currentPosition.y);
+    //Up
+    //D4 >> E6
+    //D4 >> C6
+    var canMoveInLShape = (difInX == 2 && difInY == 1) ? true : (difiny == 2 && difinx == 1) ? true : false;
+    //Down
+    //D4 >> C2
+    //D4 >> E2
+    //Left
+    //D4 >> Up B5 or Down B3
+    //Right
+    //D4 >> Up F5 or Down F3
+
+    return canMoveInLShape;
 }
+
 
 export const PieceColor = {
     White: "White",
