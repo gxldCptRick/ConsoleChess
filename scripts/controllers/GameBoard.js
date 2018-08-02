7d83499dcd31768e63354ca94d575458178ff850
ANDRES H CARRERA <andreshcar@live.com>
2018-08-01 19:44:20 -0600

    destroying the methods for the gameboard so that we can start with a clear head

--- scripts/controllers/GameBoard.js
+++ scripts/controllers/GameBoard.js
@@ -5,33 +5,4 @@ export class GameBoard {
         this.pieces = [];
         this.resetGameBoard();
     }
-
-    resetGameBoard(){
-        this.pieces = [];
-        this.addWhitePawns();
-        this.addBlackPawns();
-    }
-
-    addWhitePawns(){
-        for (let index = 0; index < 7; index++) {
-            const piece = new ChessPiece(PieceColor.White, PieceTypes.Pawn);
-            piece.setPosition(new BoardPoint(index, 2));
-            this.pieces.push(piece);
-        }
-    }
-    addSpecialDoubles(color, type, letterPosition, numberPosition)
-    {
-        for(let i = 0; i < 2; i++){
-            const piece = new ChessPiece(color, type);
-            piece.setPosition(new BoardPoint(letterPosition))
-        }
-    }
-
-    addBlackPawns(){
-        for (let index = 0; index < 7; index++) {
-            const piece = new ChessPiece(PieceColor.Black, PieceTypes.Pawn);
-            piece.setPosition(new BoardPoint(index, 7));
-            this.pieces.push(piece);
-        }
-    }
 }
