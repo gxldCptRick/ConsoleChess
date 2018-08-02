716a940576f7089dc14586466fa13a9bdcd2a795
Fernando Blanco <fern.alej.blanco@gmail.com>
2018-08-02 09:29:38 -0600

    Adding Tests for Queen

---
+++ tests/models/queen.spec.js
@@ -0,0 +1,4 @@
+const mocha = require('mocha');
+const assert = require('assert');
+const { ChessPiece, PieceTypes, PieceColor } = require('../../lib/models/ChessPiece'); 
+const { BoardPoint } = require('../../lib/models/BoardPoint');
