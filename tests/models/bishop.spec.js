1d369061e63f6856578713081f67878ecbfe8195
Fernando Blanco <fern.alej.blanco@gmail.com>
2018-08-02 09:29:50 -0600

    Adding Tests for Bishop

--- tests/models/bishop.spec.js
+++ tests/models/bishop.spec.js
@@ -5,9 +5,42 @@ const { BoardPoint } = require('../../lib/models/BoardPoint');
 
 
 mocha.describe("Bishop Movement",function(){
-    mocha.it("Bishop Can Move Diagonally", function(){
+    mocha.it("Bishop Can Move Diagonally Forward", function(){
         var chessPiece = new ChessPiece(PieceColor.White, PieceTypes.Bishop);
-        chessPiece.setPosition(new BoardPoint('A', 1));
-        assert.equal(chessPiece.moveTo(new BoardPoint('a', 7)), true);
-    }); 
-});
+        chessPiece.setPosition(new BoardPoint('C', 1));
+        assert.equal(chessPiece.moveTo(new BoardPoint('B', 2)), true);
+    });
+
+    mocha.it("Bishop Can Move Diagonally Backward", function(){
+        var chessPiece = new ChessPiece(PieceColor.White, PieceTypes.Bishop);
+        chessPiece.setPosition(new BoardPoint('C', 3));
+        assert.equal(chessPiece.moveTo(new BoardPoint('B', 2)), true);
+    });
+
+    mocha.it("Bishop Can't Move Horizontally", function(){
+        var chessPiece = new ChessPiece(PieceColor.White, PieceTypes.Bishop);
+        chessPiece.setPosition(new BoardPoint('C', 1));
+        assert.equal(chessPiece.moveTo(new BoardPoint('D', 1)), false);
+    });
+
+    mocha.it("Bishop Can't Move Vertically", function(){
+        var chessPiece = new ChessPiece(PieceColor.White, PieceTypes.Bishop);
+        chessPiece.setPosition(new BoardPoint('C', 1));
+        assert.equal(chessPiece.moveTo(new BoardPoint('C', 2)), false);
+    });
+
+    mocha.it("Bishop Can't Move To Space Its Currently On", function(){
+        var chessPiece = new ChessPiece(PieceColor.White, PieceTypes.Bishop);
+        chessPiece.setPosition(new BoardPoint('C', 1));
+        assert.equal(chessPiece.moveTo(new BoardPoint('C', 1)), false);
+    });
+
+    // mocha.it("Bishop Can Move Diagonally Forward", function(){
+    //     var chessPiece = new ChessPiece(PieceColor.White, PieceTypes.Bishop);
+    //     chessPiece.setPosition(new BoardPoint('C', 1));
+    //     assert.equal(chessPiece.moveTo(new BoardPoint('B', 2)), true);
+    // });
+
+})
+
+    
