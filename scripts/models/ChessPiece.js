193b6c8d97e6c8d362522b7cd7bec834053227a0
Fernando Blanco <fern.alej.blanco@gmail.com>
2018-08-02 15:09:26 -0600

    Finished with Rook.canMoveTo function logic (for now)

--- scripts/models/ChessPiece.js
+++ scripts/models/ChessPiece.js
@@ -46,7 +46,11 @@ PieceTypes.Pawn.canMoveTo = (currentPosition, nextPosition) => {
     return result;
 }
 PieceTypes.Rook.canMoveTo = (currentPosition, nextPosition) => {
-    return true;
+    var difInX = Math.abs(nextPosition.x - currentPosition.x);
+    var difInY = Math.abs(nextPosition.y - currentPosition.y);
+    var canMoveHorizontal = difInX > 0 && difInY == 0;
+    var canMoveVertical = difInX == 0 && difInY > 0;
+    return canMoveHorizontal || canMoveVertical;
 }
 
 //Bishop Movement
@@ -54,10 +58,6 @@ PieceTypes.Bishop.canMoveTo = (currentPosition, nextPosition) =>
 {
 var difInX = Math.abs(nextPosition.x - currentPosition.x);
 var difInY = Math.abs(nextPosition.y - currentPosition.y);
-//D4 >> C5
-//D4 >> E5
-//D4 >> C3
-//D4 >> E3
 var canMoveDiagonal = difInX > 0 && difInY > 0;
 return canMoveDiagonal;
 }
@@ -65,21 +65,9 @@ return canMoveDiagonal;
 PieceTypes.Knight.canMoveTo = (currentPosition, nextPosition) =>{
     var difInX = Math.abs(nextPosition.x - currentPosition.x);
     var difInY = Math.abs(nextPosition.y - currentPosition.y);
-    //Up
-    //D4 >> E6
-    //D4 >> C6
     var canMoveInLShape = (difInX == 2 && difInY == 1) ? true : (difInY == 2 && difInX == 1) ? true : false;
-    //Down
-    //D4 >> C2
-    //D4 >> E2
-    //Left
-    //D4 >> Up B5 or Down B3
-    //Right
-    //D4 >> Up F5 or Down F3
-
     return canMoveInLShape;
 }
-
 
 export const PieceColor = {
     White: "White",
