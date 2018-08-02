c19dee4d7939ca8b2f01adebfd50485be38ba280
Fernando Blanco <fern.alej.blanco@gmail.com>
2018-08-02 09:30:03 -0600

    Adding Tests for King

---
+++ tests/models/king.spec.js
@@ -0,0 +1,5 @@
+const mocha = require('mocha');
+const assert = require('assert');
+const { ChessPiece, PieceTypes, PieceColor } = require('../../lib/models/ChessPiece'); 
+const { BoardPoint } = require('../../lib/models/BoardPoint');
+
