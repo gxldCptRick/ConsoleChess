d019fde72ae0597901358cf8a982770c119efcd2
ANDRES H CARRERA <andreshcar@live.com>
2018-08-02 12:51:42 -0600

    creating a fileinputparser class to parse the inputs into a usable format

---
+++ scripts/controllers/FileInputParser.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const readLine = require('readline');
+var FileReader = {
+    readInputs : [],
+    processFile: (filePath) => {
+        if(typeof filePath !== 'string') throw "filePath Must be a string"
+        this.readFromFile(filePath);
+    },    
+    readFromFile: (filePath)=> {
+        var interface = readLine.createInterface({
+            input: fs.createReadStream(filePath)   
+        });
+        interface.on('line', function(lineRead){
+            var array = lineRead.split(' ');
+            this.readInputs.push(array); 
+        });
+    }
+};
+
