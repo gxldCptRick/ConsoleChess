fcd276b6c28944280e7cade07b2874e838cb2f67
ANDRES H CARRERA <andreshcar@live.com>
2018-08-02 12:51:10 -0600

    nuking the index.js file so that it can function as the proper start point

--- scripts/index.js
+++
@@ -1,6 +0,0 @@
-import { GameBoard } from './controllers/GameBoard';
-import { ChessPiece, PieceColor, PieceTypes } from './models/ChessPiece';
-var queen = new ChessPiece(PieceColor.White, PieceTypes.Queen);
-console.log(queen.type.value)
-var Board = new GameBoard();
-console.log(Board)
