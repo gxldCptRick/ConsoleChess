b93c25e0069fd7539dfa8a53580a3f081289c2ff
ANDRES H CARRERA <andreshcar@live.com>
2018-08-02 22:39:17 -0600

    finalizing the first product of our inital main

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,32 @@
 import { GameBoard } from './controllers/GameBoard';
 import { FileReader } from './controllers/FileInputParser';
+import { cursorTo } from 'readline';
+import { callbackify } from 'util';
+let path = require('path');
+let callBack = () => {
+    let processedInputs = FileReader.readInputs;
+    console.log(processedInputs);
+    let CurrentGame = new GameBoard({ 
+        x: 8,
+        y: 8
+    });
+    processedInputs.forEach(input => {
+        if(input.length === 2) {
+            try {
+               let output =  CurrentGame.runSinglePointCommand(input);
+               console.log(output);
+            } catch (e) {
+                console.error(e);
+            }
+        }else {
+            throw "What"
+        }});
+};
 
 let args = process.argv.slice(2);
 
 if(args.length !== 1) throw "No Path Provided" 
+let filePath = args[0];
+if(filePath.substring(1, 3) == ':\\' || filePath.substring(1, 3) == ':/') FileReader.processFile(filePath, callBack);
+else FileReader.processFile(path.normalize(path.join(process.cwd(), filePath)), callBack);
+
