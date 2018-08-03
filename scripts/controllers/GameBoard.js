e990485273a03dfba09de4559affac260cc63063
ANDRES H CARRERA <andreshcar@live.com>
2018-08-02 22:42:05 -0600

    refactoring the gameboard so that it uses the correct secondary command

--- scripts/controllers/GameBoard.js
+++ scripts/controllers/GameBoard.js
@@ -75,7 +75,7 @@ export class GameBoard {
 
     runSinglePointCommand(commandArgs){
         let firstPosition = commandArgs[0];
-        let secondPosition = commandArgs[0];
+        let secondPosition = commandArgs[1];
         let originPoint = this.validateInRange(firstPosition);
         let destinationPoint = this.validateInRange(secondPosition);
         let pieceAtPoint = this.findPieceBasedOnPoint(originPoint);
@@ -86,14 +86,14 @@ export class GameBoard {
 
     findPieceBasedOnPoint(originPoint){
         let piecesFound = this.pieces.filter((element) => element.getPosition().x == originPoint.x && element.getPosition().y == originPoint.y);
-        if(piecesAtPoint.length > 1) throw "too many pieces to decide which one";
-        if(piecesAtPoint.length < 1) throw "no piece at the selected point";
+        if(piecesFound.length > 1) throw "too many pieces to decide which one";
+        if(piecesFound.length < 1) throw "no piece at the selected point";
         return piecesFound[0];
     }
 
     validateInRange(positionToCheck){
         if(positionToCheck.length > 2) throw "Command To Long"
-        let point = new Point(positionToCheck[0], positionToCheck[1]);
+        let point = new BoardPoint(positionToCheck[0], positionToCheck[1]);
         if(point.x < LowerBounds.x || point.x > this.xLimit) throw "X point out of bounds"
         if(point.y < LowerBounds.y || point.y > this.yLimit) throw "Y point out of bounds"
         return point
