76de889619bb197805739a2a26ddcfacd44a784b
ANDRES H CARRERA <andreshcar@live.com>
2018-08-02 19:52:19 -0600

    adding code to process a string array into the correct commands for the dual point command

--- scripts/controllers/GameBoard.js
+++ scripts/controllers/GameBoard.js
@@ -8,17 +8,38 @@ const LowerBounds = {
 export class GameBoard {
     constructor(bounds){
         this.pieces = [];
-        this.xLimit = bounds.x
-        this.yLimit = bounds.y
+        this.xLimit = bounds.x;
+        this.yLimit = bounds.y;
+        this.setupBoard();
+    }
+
+    setupBoard(){
+
     }
 
     runSinglePointCommand(commandArgs){
-        var firstPosition = commandArgs[0];
-        var secondPosition = commandArgs[0];
-        if(firstPosition.length > 2) throw "Command Too Long"
-    
-        var firstPoint = new BoardPoint(firstPosition[0], firstPosition[1])
-        var secondPoint = new BoardPoint(secondPosition[0], secondPosition[1]);
-        
+        let firstPosition = commandArgs[0];
+        let secondPosition = commandArgs[0];
+        let originPoint = this.validateInRange(firstPosition);
+        let destinationPoint = this.validateInRange(secondPosition);
+        let pieceAtPoint = this.findPieceBasedOnPoint(originPoint);
+        let success = pieceAtPoint.moveTo(destinationPoint);
+        if(!success) throw `${pieceAtPoint.type.name} is not able to move there`
+        return `${pieceAtPoint.type.name} moved to ${destinationPoint.Point}`
+    }
+
+    findPieceBasedOnPoint(originPoint){
+        let piecesFound = this.pieces.filter((element) => element.getPosition().x == originPoint.x && element.getPosition().y == originPoint.y);
+        if(piecesAtPoint.length > 1) throw "too many pieces to decide which one";
+        if(piecesAtPoint.length < 1) throw "no piece at the selected point";
+        return piecesFound[0];
+    }
+
+    validateInRange(positionToCheck){
+        if(positionToCheck.length > 2) throw "Command To Long"
+        let point = new Point(positionToCheck[0], positionToCheck[1]);
+        if(point.x < LowerBounds.x || point.x > this.xLimit) throw "X point out of bounds"
+        if(point.y < LowerBounds.y || point.y > this.yLimit) throw "Y point out of bounds"
+        return point
     }
 }
