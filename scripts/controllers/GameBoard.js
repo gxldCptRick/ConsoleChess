526a8addf9eb3a6aee895e1c5cbcf105176799f6
ANDRES H CARRERA <andreshcar@live.com>
2018-08-02 19:25:10 -0600

    adding some code into the gameboard class

--- scripts/controllers/GameBoard.js
+++ scripts/controllers/GameBoard.js
@@ -1,8 +1,24 @@
 import { ChessPiece, PieceTypes, PieceColor } from '../models/ChessPiece'
 import { BoardPoint } from '../models/BoardPoint'
+const LowerBounds = {
+    x: 1,
+    y: 1
+};
+
 export class GameBoard {
-    constructor(){
+    constructor(bounds){
         this.pieces = [];
-        this.resetGameBoard();
+        this.xLimit = bounds.x
+        this.yLimit = bounds.y
+    }
+
+    runSinglePointCommand(commandArgs){
+        var firstPosition = commandArgs[0];
+        var secondPosition = commandArgs[0];
+        if(firstPosition.length > 2) throw "Command Too Long"
+    
+        var firstPoint = new BoardPoint(firstPosition[0], firstPosition[1])
+        var secondPoint = new BoardPoint(secondPosition[0], secondPosition[1]);
+        
     }
 }
