2f101e3790ebc12c76053e61e7288a1ba20d86b6
ANDRES H CARRERA <andreshcar@live.com>
2018-08-04 14:52:39 -0600

    refactoring the pawn tests to use chai assertions instead of assert

--- scripts/models/Pawn.spec.js
+++ scripts/models/Pawn.spec.js
@@ -1,55 +1,88 @@
 import mocha from 'mocha';
-import assert from 'assert';
-import { PieceTypes } from './PieceTypes';
-import { ChessPiece, PieceColor } from './ChessPiece'; 
+import chai from 'chai';
+import { PieceTypes } from './PieceTypes'; 
 import BoardPoint from './BoardPoint';
 
+const should = chai.should();
+
 mocha.describe('Pawn Moves Correctly', function(){
-    mocha.it('Pawn Can Move two times on first move', function(){
-        var chessPiece = new ChessPiece(PieceColor.White, PieceTypes.Pawn);
-        chessPiece.setPosition(new BoardPoint('E',4));
-        assert.equal(chessPiece.moveTo(new BoardPoint('E', 6)), true);
+    mocha.it('Pawn Can Move Two Places Forward', function(){
+        let originalPoint = new BoardPoint('E', 4);
+        let nextPoint = new BoardPoint('E', 6);
+        let result = PieceTypes.Pawn.canMoveTo(originalPoint, nextPoint);
+        should.exist(result);
+        result.should.equal(true);
     });
     
-    mocha.it('Pawn Cant Move Three Times on first move', function(){
-        var chessPiece = new ChessPiece(PieceColor.White, PieceTypes.Pawn);
-        chessPiece.setPosition(new BoardPoint('D', 3));
-        assert.equal(chessPiece.moveTo(new BoardPoint('D', 6)), false);
+    mocha.it("Pawn Can't Move Three Spaces", function(){
+        let originalPoint = new BoardPoint('E', 4);
+        let nextPoint = new BoardPoint('E', 7);
+        let result = PieceTypes.Pawn.canMoveTo(originalPoint, nextPoint);
+        should.exist(result);
+        result.should.not.equal(true);
     });
 
-    mocha.it("Pawn Can't Move Diagonally", function(){
-        var chessPiece = new ChessPiece(PieceColor.White, PieceTypes.Pawn);
-        chessPiece.setPosition(new BoardPoint('D', 3));
-        assert.equal(chessPiece.moveTo(new BoardPoint('C', 4)), false);
+    mocha.it("Pawn Can't Move Diagonally Forward Right", function(){
+        let originalPoint = new BoardPoint('E', 4);
+        let nextPoint = new BoardPoint('F', 5);
+        let result = PieceTypes.Pawn.canMoveTo(originalPoint, nextPoint);
+        should.exist(result);
+        result.should.not.equal(true);
     });
 
-    mocha.it("Pawn Can't Move Horizontally", function(){
-        var chessPiece = new ChessPiece(PieceColor.White, PieceTypes.Pawn);
-        chessPiece.setPosition(new BoardPoint('F', 3));
-        assert.equal(chessPiece.moveTo(new BoardPoint('G', 3)), false);
+    mocha.it("Pawn Can't Move Diagonally Backward Right", function(){
+        let originalPoint = new BoardPoint('E', 4);
+        let nextPoint = new BoardPoint('F', 3);
+        let result = PieceTypes.Pawn.canMoveTo(originalPoint, nextPoint);
+        should.exist(result);
+        result.should.not.equal(true);
     });
 
-    mocha.it("Pawn Can Move One Step Forward", function(){
-        var chessPiece = new ChessPiece(PieceColor.White, PieceTypes.Pawn);
-        chessPiece.setPosition(new BoardPoint('F', 3));
-        assert.equal(chessPiece.moveTo(new BoardPoint('F', 4)), true);
+    mocha.it("Pawn Can't Move Diagonally Forward Left", function(){
+        let originalPoint = new BoardPoint('E', 4);
+        let nextPoint = new BoardPoint('D', 5);
+        let result = PieceTypes.Pawn.canMoveTo(originalPoint, nextPoint);
+        should.exist(result);
+        result.should.not.equal(true);
+    });
+
+    mocha.it("Pawn Can't Move Diagonally Backward Left", function(){
+        let originalPoint = new BoardPoint('E', 4);
+        let nextPoint = new BoardPoint('D', 3);
+        let result = PieceTypes.Pawn.canMoveTo(originalPoint, nextPoint);
+        should.exist(result);
+        result.should.not.equal(true);
+    });
+
+    mocha.it("Pawn Can't Move To The Left", function(){
+        let originalPoint = new BoardPoint('E', 4);
+        let nextPoint = new BoardPoint('D', 4);
+        let result = PieceTypes.Pawn.canMoveTo(originalPoint, nextPoint);
+        should.exist(result);
+        result.should.not.equal(true);
+    });
+
+    mocha.it("Pawn Can't Move To The Right", function(){
+        let originalPoint = new BoardPoint('E', 4);
+        let nextPoint = new BoardPoint('F', 4);
+        let result = PieceTypes.Pawn.canMoveTo(originalPoint, nextPoint);
+        should.exist(result);
+        result.should.not.equal(true);
+    });
+
+    mocha.it("Pawn Can Move Forward By One Step", function(){
+        let originalPoint = new BoardPoint('E', 4);
+        let nextPoint = new BoardPoint('E', 5);
+        let result = PieceTypes.Pawn.canMoveTo(originalPoint, nextPoint);
+        should.exist(result);
+        result.should.equal(true);
     });
 
     mocha.it("Pawn Can't Move to Same Space that it is currently on", function(){
-        var chessPiece = new ChessPiece(PieceColor.White, PieceTypes.Pawn);
-        chessPiece.setPosition(new BoardPoint('E', 3));
-        assert.equal(chessPiece.moveTo(new BoardPoint('E', 3)), false);
-    });
-
-    mocha.it("Black Pawn Can Move Forward (One Space)", function(){
-        var chessPiece = new ChessPiece(PieceColor.Black, PieceTypes.Pawn);
-        chessPiece.setPosition(new BoardPoint('E', 7));
-        assert.equal(chessPiece.moveTo(new BoardPoint('E', 6)), true);
-    });
-
-    mocha.it("Black Pawn Can Move Forward (One Space)", function(){
-        var chessPiece = new ChessPiece(PieceColor.Black, PieceTypes.Pawn);
-        chessPiece.setPosition(new BoardPoint('E', 7));
-        assert.equal(chessPiece.moveTo(new BoardPoint('E', 5)), true);
+        let originalPoint = new BoardPoint('E', 4);
+        let nextPoint = new BoardPoint('E', 4);
+        let result = PieceTypes.Pawn.canMoveTo(originalPoint, nextPoint);
+        should.exist(result);
+        result.should.not.equal(true);
     });
 });
