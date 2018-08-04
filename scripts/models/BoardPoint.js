91677bd42e0480365068b9a7360a236952be8cff
ANDRES H CARRERA <andreshcar@live.com>
2018-08-04 16:54:59 -0600

    refactoring the boardpoint to do what it is told

--- scripts/models/BoardPoint.js
+++ scripts/models/BoardPoint.js
@@ -1,16 +1,19 @@
-const ValueForAMinusOne = 64;
+const ValueForA = 65;
 export default class BoardPoint {
     constructor(alphaCharacter, numberedPosition){
         if(typeof alphaCharacter === 'string'){
             var alphalpha = alphaCharacter.toUpperCase();
-            this.x = alphalpha.charCodeAt(0) - ValueForAMinusOne;
+            this.x = alphalpha.charCodeAt(0) - ValueForA;
         }
-        else this.x = alphaCharacter + 1;
+        else{
+            this.x = alphaCharacter;  
+        } 
+
         if(typeof numberedPosition === 'string') numberedPosition = parseInt(numberedPosition);
         this.y = numberedPosition;
     }
     
     get Point(){
-        return String.fromCharCode(this.x + ValueForAMinusOne) + this.y.toString();
+        return String.fromCharCode(this.x + ValueForA) + this.y.toString();
     }
 }
