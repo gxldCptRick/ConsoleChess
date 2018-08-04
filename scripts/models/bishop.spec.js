8bebde183cd8f161a0d600110baf8eaa63f2e6be
ANDRES H CARRERA <andreshcar@live.com>
2018-08-04 14:03:54 -0600

    finishing the refactoring of the bishop unit tests to use chai instead of assert

--- scripts/models/bishop.spec.js
+++ scripts/models/bishop.spec.js
@@ -14,42 +14,54 @@ mocha.describe("Bishop Moves Correctly",function(){
         result.should.equal(true);
     });
 
-    // mocha.it("Bishop Can Move Diagonally Forward Right", function(){
-    //     var chessPiece = new ChessPiece(PieceColor.White, PieceTypes.Bishop);
-    //     chessPiece.setPosition(new BoardPoint('D', 4));
-    //     assert.equal(chessPiece.moveTo(new BoardPoint('E', 5)), true);
-    // });
+    mocha.it("Bishop Can Move Diagonally Forward Right", function(){
+        let originalPoint = new BoardPoint('D', 4);
+        let nextPoint = new BoardPoint('E', 5);
+        let result = PieceTypes.Bishop.canMoveTo(originalPoint, nextPoint);
+        should.exist(result);
+        result.should.equal(true);
+    });
 
-    // mocha.it("Bishop Can Move Diagonally Backward Left", function(){
-    //     var chessPiece = new ChessPiece(PieceColor.White, PieceTypes.Bishop);
-    //     chessPiece.setPosition(new BoardPoint('D', 4));
-    //     assert.equal(chessPiece.moveTo(new BoardPoint('C', 3)), true);
-    // });
+    mocha.it("Bishop Can Move Diagonally Backward Left", function(){
+        let originalPoint = new BoardPoint('D', 4);
+        let nextPoint = new BoardPoint('c', 3);
+        let result = PieceTypes.Bishop.canMoveTo(originalPoint, nextPoint);
+        should.exist(result);
+        result.should.equal(true);
+    });
 
     
-    // mocha.it("Bishop Can Move Diagonally Backward Right", function(){
-    //     var chessPiece = new ChessPiece(PieceColor.White, PieceTypes.Bishop);
-    //     chessPiece.setPosition(new BoardPoint('D', 4));
-    //     assert.equal(chessPiece.moveTo(new BoardPoint('E', 3)), true);
-    // });
+    mocha.it("Bishop Can Move Diagonally Backward Right", function(){
+        let originalPoint = new BoardPoint('D', 4);
+        let nextPoint = new BoardPoint('E', 3);
+        let result = PieceTypes.Bishop.canMoveTo(originalPoint, nextPoint);
+        should.exist(result);
+        result.should.equal(true);
+    });
 
-    // mocha.it("Bishop Can't Move Horizontally", function(){
-    //     var chessPiece = new ChessPiece(PieceColor.White, PieceTypes.Bishop);
-    //     chessPiece.setPosition(new BoardPoint('C', 1));
-    //     assert.equal(chessPiece.moveTo(new BoardPoint('D', 1)), false);
-    // });
+    mocha.it("Bishop Can't Move Horizontally", function(){
+        let originalPoint = new BoardPoint('D', 4);
+        let nextPoint = new BoardPoint('E', 4);
+        let result = PieceTypes.Bishop.canMoveTo(originalPoint, nextPoint);
+        should.exist(result);
+        result.should.not.equal(true);
+    });
 
-    // mocha.it("Bishop Can't Move Vertically", function(){
-    //     var chessPiece = new ChessPiece(PieceColor.White, PieceTypes.Bishop);
-    //     chessPiece.setPosition(new BoardPoint('C', 1));
-    //     assert.equal(chessPiece.moveTo(new BoardPoint('C', 2)), false);
-    // });
+    mocha.it("Bishop Can't Move Vertically", function(){
+        let originalPoint = new BoardPoint('D', 4);
+        let nextPoint = new BoardPoint('D', 5);
+        let result = PieceTypes.Bishop.canMoveTo(originalPoint, nextPoint);
+        should.exist(result);
+        result.should.not.equal(true);
+    });
 
-    // mocha.it("Bishop Can't Move To Space Its Currently On", function(){
-    //     var chessPiece = new ChessPiece(PieceColor.White, PieceTypes.Bishop);
-    //     chessPiece.setPosition(new BoardPoint('C', 1));
-    //     assert.equal(chessPiece.moveTo(new BoardPoint('C', 1)), false);
-    // });
+    mocha.it("Bishop Can't Move To Space Its Currently On", function(){
+        let originalPoint = new BoardPoint('D', 4);
+        let nextPoint = new BoardPoint('D', 4);
+        let result = PieceTypes.Bishop.canMoveTo(originalPoint, nextPoint);
+        should.exist(result);
+        result.should.not.equal(true);
+    });
 
 })
 
