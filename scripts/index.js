39d4ab04bf8bd5262090c74bfb12ee94d70ea63c
ANDRES H CARRERA <andreshcar@live.com>
2018-08-04 18:35:06 -0600

    refactoring the main to actually use the board display. fixing a small bug in which the board would be filled in naturally and have the user specific stuff layerd on top

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,13 @@
 /* eslint-disable no-console */
-
+import { BoardDisplay } from './views/BoardDisplay';
 import { GameBoard } from './controllers/GameBoard';
 import { FileReader } from './controllers/FileReader';
 
 let path = require('path');
 let fileReader = new FileReader();
+let CurrentGame = null;
 let callBack = (processedInputs) => {
-    let CurrentGame = new GameBoard({ 
-        x: 8,
-        y: 8
-    });
+    let display = new BoardDisplay(CurrentGame);
     processedInputs.forEach((input, index) => {
         if(input.length === 2) {
             try {
@@ -26,6 +24,7 @@ let callBack = (processedInputs) => {
             console.log(input);
             console.log(index);
         }
+        display.displayBoard();
     });
 };
 
@@ -35,9 +34,18 @@ if(args.length < 1) throw "No Path Provided";
 if(args.lenght > 2) throw "Too many arguments passed"; 
 let movementFilePath = args[0];
 if(args.length > 1){
+    CurrentGame = new GameBoard({ 
+        x: 8,
+        y: 8
+    }, true);
     let placementFilePath = args[1];
     if(placementFilePath.substring(1, 3) == ':\\' || movementFilePath.substring(1, 3) == ':/') fileReader.processFile(placementFilePath, callBack);
     else fileReader.processFile(path.normalize(path.join(process.cwd(), placementFilePath)), callBack);
+}else{
+    CurrentGame = new GameBoard({ 
+        x: 8,
+        y: 8
+    }, false);
 }
 
 if(movementFilePath.substring(1, 3) == ':\\' || movementFilePath.substring(1, 3) == ':/') fileReader.processFile(movementFilePath, callBack);
