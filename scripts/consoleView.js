bd259438ded28361130b223d137f17749b2a155f
ANDRES H CARRERA <andreshcar@live.com>
2018-08-06 11:02:36 -0600

    removing the scripts folder

--- scripts/consoleView.js
+++
@@ -1,6 +0,0 @@
-import  { BoardDisplay } from './views/BoardDisplay'
-import { GameBoard } from './controllers/GameBoard'
-let boardToBeDisplayed = new GameBoard({x: 8, y:8});
-let boardDisplayer = new BoardDisplay(boardToBeDisplayed);
-
-boardDisplayer.displayBoard();
