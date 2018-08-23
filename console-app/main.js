a23270b6744c1761f196b06bc9d28dc41860cf0f
ANDRES H CARRERA <andreshcar@live.com>
2018-08-23 09:25:00 -0600

    refactoring the main to contain ChessConsoleProccessor Class to do the flow of controll of the application

--- console-app/main.js
+++ console-app/main.js
@@ -2,52 +2,72 @@
 import { BoardDisplay } from './views/BoardDisplay';
 import { GameBoard } from '../lib/controllers/GameBoard';
 import { FileReader } from '../lib/controllers/FileReader';
+import path from 'path';
 
-let path = require('path');
-let fileReader = new FileReader();
-let CurrentGame = null;
-let callBack = (processedInputs) => {
-    let display = new BoardDisplay(CurrentGame);
-    processedInputs.forEach((input, index) => {
-        if(input.length === 2) {
-            try {
-               let output =  CurrentGame.runSinglePointCommand(input);
-               console.log(output);
-            } catch (e) {
-                console.error(e);
-            }
-        }else if(input.length === 1){
-            let output = CurrentGame.runPlacementCommand(input[0]);
-            console.log(output);
+export class ChessConsoleProccessor {
+    constructor(pathMovement, pathPlacement) {
+        this.fileReader = new FileReader();
+        this.filePaths = [pathMovement];
+        if (pathPlacement) {
+            this.currentGame = new GameBoard({ x: 8, y: 8 }, true);
+            this.filePaths.push(pathPlacement);
+        } else {
+            this.currentGame = new GameBoard();
         }
-        else {
-            console.log(input);
-            console.log(index);
+    }
+    
+    start(){
+        this.proccessFilePaths(this.filePaths);
+    }
+
+    proccessFilePaths(movements) {
+        let movementFilePath = this.generatePath(movements[0]);
+        if (movements.length > 1 && movements.length < 2) {
+            let placementPath = this.generatePath(movements[0]);
+            this.fileReader.processFile(placementPath, this.processTheInputs(this));
         }
-        display.displayBoard();
-    });
-};
+
+        this.fileReader.processFile(movementFilePath, this.processTheInputs(this));
+    }
+
+    generatePath(roughPath) {
+        let fullPath = null;
+        if (roughPath.includes(':\\') || roughPath.includes(':/')) fullPath = roughPath;
+        else fullPath = path.join(process.cwd(), roughPath);
+        return fullPath;
+    }
+
+    processTheInputs(context) {
+        return (processedInputs) => {
+            let display = new BoardDisplay(this.currentGame);
+            processedInputs.forEach((input, index) => {
+                if (input.length === 2) {
+                    try {
+                        let output = context.currentGame.runSinglePointCommand(input);
+                        console.log(output);
+                    } catch (e) {
+                        console.error(e);
+                    }
+                } else if (input.length === 1) {
+                    let output = context.currentGame.runPlacementCommand(input[0]);
+                    console.log(output);
+                }
+                else {
+                    console.log(input);
+                    console.log(index);
+                }
+                display.displayBoard();
+            });
+        }
+    }
+}
 
 let args = process.argv.slice(2);
 
-if(args.length < 1) throw "No Path Provided";
-if(args.lenght > 2) throw "Too many arguments passed"; 
-let movementFilePath = args[0];
-if(args.length > 1){
-    CurrentGame = new GameBoard({ 
-        x: 8,
-        y: 8
-    }, true);
-    let placementFilePath = args[1];
-    if(placementFilePath.substring(1, 3) == ':\\' || movementFilePath.substring(1, 3) == ':/') fileReader.processFile(placementFilePath, callBack);
-    else fileReader.processFile(path.normalize(path.join(process.cwd(), placementFilePath)), callBack);
-}else{
-    CurrentGame = new GameBoard({ 
-        x: 8,
-        y: 8
-    }, false);
-}
-
-if(movementFilePath.substring(1, 3) == ':\\' || movementFilePath.substring(1, 3) == ':/') fileReader.processFile(movementFilePath, callBack);
-else fileReader.processFile(path.normalize(path.join(process.cwd(), movementFilePath)), callBack);
-
+if (args.length === 1){
+    let controller = new ChessConsoleProccessor(args[0]);
+    controller.start();
+}else if (args.length === 2){
+    let controller = new ChessConsoleProccessor(args[0], args[1]);
+    controller.start();
+}else throw new Error('Too Many Args. Please Pass In only a movementfile and optionally a placement file');
